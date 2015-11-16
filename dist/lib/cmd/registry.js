bb0ff870c9449e0af53c5316c09dd032715b5e09
Erlimar Pereira Silva Campos <erlimar.campos@basis.com.br>
2015-11-16 19:43:44 -0200

    Added registry DevCom

---
+++ dist/lib/cmd/registry.js
@@ -0,0 +1,25 @@
+// Copyright (c) E5R Development Team. All rights reserved.
+// Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
+
+/* global process, __filename, __dirname */
+
+var dev = require('dev');
+
+/**
+ * DevCom `registry` command
+ * @class
+ * 
+ * Manage registry of E5R Tools for Development Team
+ */
+class Registry extends dev.DevCom {
+    
+    /**
+     * Run the `registry` devcom
+     * 
+     * @param {DevToolCommandLine} toolInstance - Instance of DevToolCommandLine
+     * @param {Array} args - Argument list
+     */
+    run(toolInstance, args) {
+        dev.logger.verbose('Running [REGISTRY] DevCom...');
+    }
+}
