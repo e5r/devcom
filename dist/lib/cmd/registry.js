2c81c9990cab0a5a2e2e4a785e017a39f85d2a55
Erlimar Pereira Silva Campos <erlimar.campos@basis.com.br>
2015-11-17 12:16:57 -0200

    Log messages added

--- dist/lib/cmd/registry.js
+++ dist/lib/cmd/registry.js
@@ -4,7 +4,19 @@
 /* global process, __filename, __dirname */
 
 console.log('require(\'dev\') start...');
+console.log('PATHS: ', module.paths);
+
+console.log('Verify exists file DEV.JS');
+for(var p in module.paths){
+    var fs = require('fs'),
+        path = require('path'),
+        filePath = path.resolve(module.paths[p], 'dev.js');
+        
+    console.log(filePath, '=>', fs.existsSync(filePath));
+}
+
 var dev = require('dev');
+
 console.log('require(\'dev\') finish...:', dev);
 
 /**
