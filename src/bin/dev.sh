c35e19e61550f5f8f864a4496ef523eee065cccb
Erlimar Silva Campos <erlimar@gmail.com>
2015-11-21 13:01:35 -0200

    Added incompleted binary `dev.sh`

---
+++ src/bin/dev.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Copyright (c) E5R Development Team. All rights reserved.
+# Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
+
+echo "TODO: dev not implemented for Unix"
