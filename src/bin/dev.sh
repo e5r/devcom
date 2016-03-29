5b54a0f2a531eb9a93c85bca3ab621d88ae40df3
Erlimar Pereira Silva <erlimar@debian>
2016-03-29 18:58:55 -0300

    Partial implementation of *nix dev command

--- src/bin/dev.sh
+++ src/bin/dev.sh
@@ -1,6 +1,26 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Copyright (c) E5R Development Team. All rights reserved.
 # Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
 
-echo "TODO: dev not implemented for Unix"
+localdir="`dirname \`readlink -f "${0}"\``"
+jsengine="${localdir}/../tools/jsengine"
+devscript="${localdir}/../lib/node_modules/e5r-dev.js"
+postfile="${localdir}/../tools/dev-envvars.sh"
+
+if [ ! -f ${jsengine} ]; then
+    echo "${jsengine} not found!"
+    echo "Run installer for E5R Tools for Development Team before."   
+    return 1
+fi
+
+# @HACK: Update environment after install
+if ! "${1}" = "update-devenvvars"; then
+    ${jsengine} "${devscript}" ${*} --shell=sh
+fi
+
+# Update environment from postfile
+if [ -f "${postfile}" ]; then
+    source "${postfile}"
+    rm -f "${postfile}"
+fi
