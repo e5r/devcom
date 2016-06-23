bc13ba15c55cba8bd37afcca43f8001a4a8add6a
Erlimar Silva Campos <erlimar@gmail.com>
2016-06-23 17:18:40 -0300

    Partial implements

--- src/lib/cmd/init.js
+++ src/lib/cmd/init.js
@@ -4,19 +4,18 @@
 /* global process, __filename, __dirname */
 "use strict";
 
-let _fs = require('fs'),
+let _os = require('os'),
     _path = require('path'),
-    _url = require('url'),
-    _os = require('os'),
-    dev = require('e5r-dev');
+    _crypto = require('crypto'),
+    _dev = require('e5r-dev');
 
 /**
  * DevCom `init` command
  * @class
  * 
- * Init new project. Start E5R New Project Wizard.
+ * Start E5R New Project Wizard.
  */
-class Init extends dev.DevCom {
+class Init extends _dev.DevCom {
 
     /**
      * Run the `init` devcom
@@ -25,13 +24,12 @@ class Init extends dev.DevCom {
      * @param {object} options - Options for arguments of command
      */
     run(devTool, options) {
-        if ((process.env['DEVCOM_MODE'] || '').toUpperCase() !== 'DEVELOPMENT' && !(devTool instanceof dev.DevTool)) {
-            throw dev.createError('Init should be performed only via DEV command.');
+        if ((process.env['DEVCOM_MODE'] || '').toUpperCase() !== 'DEVELOPMENT' && !(devTool instanceof _dev.DevTool)) {
+            throw _dev.createError('Init should be performed only via DEV command.');
         }
 
-        // 1. Check parameter [0] format. githubuser/repository@version
-        //    @version is optional, default is master
-        //    Ex: e5r/empty-project@1.0.0 -> https://github.com/e5r/empty-project/archive/v1.0.0.zip
+        // Check parameter [0] format. githubuser/repository@version
+        // - @version is optional, default is master
         if (!options || !Array.isArray(options.args) || 1 > options.args.length) {
             this.usage();
             return;
@@ -41,36 +39,48 @@ class Init extends dev.DevCom {
             template = r.exec(options.args[0]);
 
         if (!template) {
-            throw dev.createError('Invalid template format.');
+            throw _dev.createError('Invalid template format.');
         }
 
         let user = template[1],
             repository = template[2],
             version = template[4] || 'master',
-            workdir = options.workdir || process.cwd();
+            workdir = options.workdir || process.cwd(),
+            zipFileName = '{version}.zip'.replace('{version}', version);
 
-        let urlGitHub = 'https://github.com/{user}/{repository}/archive/{version}.zip'
+        // Create if --workdir not exists
+        if (!_dev.directoryExists(workdir)) {
+            _dev.mkdir(workdir);
+        }
+
+        // Up exception if --workdir is not empty
+        if (!_dev.directoryIsEmpty(workdir)) {
+            throw _dev.createError('Directory [' + workdir + '] is not empty.');
+        }
+
+        // Generate GitHub URL. 
+        // - Ex: e5r/empty-project@1.0.0 -> https://codeload.github.com/e5r/empty-project/zip/v1.0.0
+        let urlGitHub = 'https://codeload.github.com/{user}/{repository}/zip/{version}'
             .replace('{user}', user)
             .replace('{repository}', repository)
-            .replace('{version}', version);
+            .replace('{version}', version),
 
-        // 2. Create if --workdir not exists
-        if (!dev.directoryExists(workdir)) {
-            dev.mkdir(workdir);
+            tmpPath = _dev.generateTempDir(),
+            zipPath = _path.join(tmpPath, zipFileName);
+
+        if (!_dev.directoryExists(tmpPath)) {
+            _dev.mkdir(tmpPath);
         }
 
-        // 3. Up exception if --workdir is not empty
-        if (!dev.directoryIsEmpty(workdir)) {
-            throw dev.createError('Directory [' + workdir + '] is not empty.');
-        }
+        // Download ZIP file to temporary directory
+        _dev.printf('TMP:', tmpPath);
+
+        _dev.downloadSync(urlGitHub, zipPath);
+
+        // 6. Extract ZIP file
 
         let pause = true;
 
-        // 4. Get a temporary directory
-
-        // 5. Download ZIP file to temporary directory
-
-        // 6. Extract ZIP file
 
         // 7. Read '.init-template.json' from temporary directory
 
@@ -81,13 +91,21 @@ class Init extends dev.DevCom {
      * Show usage information for DevCom
      */
     usage() {
-        dev.printf('Usage: dev init [options]');
-        dev.printf();
-        dev.printf('  Options:');
-        dev.printf('    template       - Template signature: Ex: <user>/<repository>[@<version>]');
-        dev.printf('      > user       - GitHub user or organization name');
-        dev.printf('      > repository - GitHub repository name');
-        dev.printf('      > version    - Branch/Tag name. Default: master');
+        _dev.printf('Usage: _dev init [options]');
+        _dev.printf();
+        _dev.printf('  Options:');
+        _dev.printf('    template       - Template signature: Ex: <user>/<repository>[@<version>]');
+        _dev.printf('      > user       - GitHub user or organization name');
+        _dev.printf('      > repository - GitHub repository name');
+        _dev.printf('      > version    - Branch/Tag name. Default: master');
+    }
+
+    /**
+     * Generate a temporary directory name
+     */
+    getTempDir() {
+        let tmpdir = _crypto.randomBytes(16).toString('hex');
+        return _path.join(_os.tmpdir(), tmpdir);
     }
 }
 
@@ -96,14 +114,14 @@ module.exports = new Init();
 // devcom.run(this, parseArgOptions(this._args));
 // Run Init DevCom on developer instance
 if (!module.parent && module.filename === __filename) {
-    let _devTool = dev.devToolDefaultInstance,
+    let _devTool = _dev.devToolDefaultInstance,
         _devCom = module.exports,
-        _options = dev.parseOptions(process.argv.slice(2));
+        _options = _dev.parseOptions(process.argv.slice(2));
 
     try {
         _devCom.run(_devTool, _options);
     } catch (error) {
-        dev.logger.error(error);
+        _dev.logger.error(error);
         _devTool.exitCode = error.code || 1;
     }
 
