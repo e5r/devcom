9667953b96f3a2c8daefe9eb2dec96d1302351f5
Erlimar Silva Campos <erlimar@gmail.com>
2017-01-13 10:48:34 -0200

    DEVCOM env reformulated. NODE env implemented partial for macOS.

--- src/lib/cmd/env.js
+++ src/lib/cmd/env.js
@@ -6,7 +6,9 @@
 
 let _dev = require('e5r-dev'),
     _path = require('path'),
-    _fs = require('fs');
+    _url = require('url'),
+    _fs = require('fs'),
+    _os = require('os');
 
 /** @constant {string} */
 const CACHE_FOLDER = 'cache';
@@ -38,6 +40,52 @@ const ALIAS = {
         'doc': 'Checks if the version is installed'
     }
 };
+
+/**
+ * Get a current architecture
+ */
+function getArchitecture() {
+    let arch = _os.arch();
+
+    if (arch === 'ia32') {
+        return 'x86';
+    }
+
+    return arch;
+}
+
+/**
+ * Get a current platform
+ */
+function getPlatform() {
+    let platform = _os.platform();
+
+    if (platform === 'darwin') {
+        return 'osx';
+    }
+
+    if (platform === 'win32') {
+        return 'win';
+    }
+
+    return platform;
+}
+
+/**
+ * Make a installation directory path to a version
+ * 
+ * @param {string} version - Version number
+ * @param {string} envName - Name of environment
+ */
+function makeInstallDirectoryPath(version, envName) {
+    let installDirectoryRoot = _path.join(_dev.devHome.root, 'env', envName);
+
+    if (!_dev.directoryExists(installDirectoryRoot)) {
+        _dev.mkdir(installDirectoryRoot);
+    }
+
+    return _path.join(installDirectoryRoot, version);
+}
 
 /**
  * Ensures that 'action' is an expected name.
@@ -116,9 +164,370 @@ function saveVersionCacheInfo(envName, cache) {
         _dev.mkdir(cacheDirPath);
     }
 
+    prepareVersionInfo(cache, envName);
+
     _fs.writeFileSync(versionInfoFilePath, JSON.stringify(cache || {}, null, 4), 'utf8');
 
     return require(versionInfoFilePath);
+}
+
+/**
+ * Remove a object propertie if exists
+ * 
+ * @param {object[]} objList - The object list
+ * @param {string[]} propList - The propertie name list
+ */
+function removeProperty(objList, propList) {
+    if (!Array.isArray(objList) || !Array.isArray(propList)) {
+        return;
+    }
+
+    objList.map(obj => propList.map(prop => {
+        if (Object.getOwnPropertyDescriptor(obj, prop)) {
+            delete obj[prop];
+        }
+    }));
+}
+
+/**
+ * Prepare a platform and architecture object properties.
+ * 
+ * @param {object} objList - The object list
+ */
+function preparePlatformArchitectureObject(objList) {
+    if (!Array.isArray(objList)) {
+        objList = [objList];
+    }
+
+    objList.map(obj => {
+        if (!Array.isArray(obj.files)) {
+            return;
+        }
+
+        if (typeof obj.platforms !== 'object') {
+            obj.platforms = {};
+        }
+
+        obj.files.map(file => {
+            let parts = new RegExp('^(\\w+)-(\\w+).*$', 'g').exec(file);
+
+            if (!Array.isArray(parts)) {
+                return;
+            }
+
+            let platform = parts[1],
+                arch = parts[2];
+
+            if (!Array.isArray(obj.platforms[platform])) {
+                obj.platforms[platform] = [];
+            }
+
+            if (0 > obj.platforms[platform].indexOf(arch)) {
+                obj.platforms[platform].push(arch);
+            }
+        });
+    });
+}
+
+/**
+ * Short and validate version info
+ * 
+ * @param {object} versionInfo - Version information
+ * @param {string} envName - Name of environment
+ */
+function prepareVersionInfo(versionInfo, envName) {
+    const INVALID_MESSAGE = 'Invalid ' + envName.toUpperCase() + ' version information format.';
+
+    if (versionInfo.environment !== envName) {
+        throw _dev.createError(INVALID_MESSAGE);
+    }
+
+    if (!Array.isArray(versionInfo.versions)) {
+        throw _dev.createError(INVALID_MESSAGE);
+    }
+
+    let finalVersions = [];
+
+    // Sort by version number DESC
+    // Prepare platform and architecture information
+    // Remove unnecessary information. (e.g: files, openssl, npm, etc.)
+    versionInfo.versions.sort((a, b) => {
+        if (typeof a.version !== 'string' || typeof b.version !== 'string') {
+            throw _dev.createError(INVALID_MESSAGE);
+        }
+
+        if ((a.files && !Array.isArray(a.files)) || (b.files && !Array.isArray(b.files))) {
+            throw _dev.createError(INVALID_MESSAGE);
+        }
+
+        // Prepare platform and architecture information
+        preparePlatformArchitectureObject([a, b]);
+
+        // Remove unnecessary information. (e.g: files, openssl, npm, etc.)
+        removeProperty([a, b], [
+            'date',
+            'npm',
+            'v8',
+            'uv',
+            'zlib',
+            'openssl',
+            'modules',
+            'lts',
+            'files'
+        ]);
+
+        // Sort by version number DESC
+        let aParts = a.version.split('.').splice(0, 3),
+            bParts = b.version.split('.').splice(0, 3);
+
+        aParts = aParts.concat(new Array(1 + 3 - aParts.length)
+            .join('0').split(''))
+            .map((v) => parseInt(v || '0', 10));
+
+        bParts = bParts.concat(new Array(1 + 3 - bParts.length)
+            .join('0')
+            .split('')).map((v) => parseInt(v || '0', 10));
+
+        let diff = 0, idx = 0;
+
+        do {
+            diff = bParts[idx] - aParts[idx];
+            idx++;
+        } while (diff === 0 && idx < 3);
+
+        return diff;
+    });
+
+    let validVersions = [];
+
+    versionInfo.versions.map(version => {
+        if (Object.getOwnPropertyNames(version.platforms).length > 0) {
+            validVersions.push(version);
+        }
+    });
+
+    versionInfo.versions = validVersions;
+}
+
+/**
+ * Make a semver valid version string
+ * 
+ * @param {string} requiredVersion - Partial version number
+ * @param {object} versionInfo - All version available
+ * @param {object} options - Options
+ * @param {object} engine - The environment engine
+ */
+function getFullVersionNumber(requiredVersion, versionInfo, options, engine) {
+    if (!Array.isArray(versionInfo.versions) || versionInfo.versions.length < 1) {
+        return null;
+    }
+
+    requiredVersion = requiredVersion === 'latest' ? null : requiredVersion;
+
+    let resultVersion = null,
+        requiredVersionParts = requiredVersion === null
+            ? []
+            : requiredVersion
+                .split('.')
+                .map(v => parseInt(v, 10)),
+        platform = getPlatform(),
+        arch = options.arch || getArchitecture(),
+        versionIsValidFn = engine['versionIsValid'];
+
+    if (typeof (versionIsValidFn) != 'function') {
+        throw _dev.createError('Environment '
+            + engine.name.toUpperCase() + ' does not implements versionIsValid() method.');
+    }
+
+    for (let idx = 0; idx < versionInfo.versions.length; idx++) {
+        let version = versionInfo.versions[idx],
+            versionParts = version.version.split('.').map(v => parseInt(v, 10)),
+            versionPlatform = version.platforms[platform];
+
+        /*
+         * @note: 1. required version > version THEN required version not found
+         *        2. version > required version THEN skip to next
+         */
+        let notFound = false,
+            skip = false;
+
+        requiredVersionParts.map((v, i) => {
+            if (notFound || skip) {
+                return;
+            }
+            if (v > versionParts[i]) {
+                notFound = true;
+                return;
+            }
+            if (versionParts[i] > v) {
+                skip = true;
+                return;
+            }
+        });
+
+        if (notFound) {
+            break;
+        }
+
+        if (skip) {
+            continue;
+        }
+
+        if (Array.isArray(versionPlatform) &&
+            versionPlatform.indexOf(arch) >= 0 &&
+            versionIsValidFn.bind(engine)(version)) {
+
+            resultVersion = version.version;
+            break;
+        }
+    }
+
+    if (resultVersion) {
+        let getFullVersionNumberFn = engine['getFullVersionNumber'];
+
+        if (typeof (getFullVersionNumberFn) != 'function') {
+            throw _dev.createError('Environment '
+                + engine.name.toUpperCase() + ' does not implements getFullVersionNumber() method.');
+        }
+
+        return getFullVersionNumberFn.bind(engine)(resultVersion);
+    }
+
+    return null;
+}
+
+/**
+ * Split filename into name and extensio
+ * 
+ * @param {string} fileName - The file name
+ * 
+ * @return {string[]} Array with 2 itens; 0 is a name and 1 is a extension or null
+ */
+function splitFileNameExtension(fileName) {
+    let reg = new Array(2),
+        knowExtensions = ['tar.xz', 'tar.gz'],
+        found = false;
+
+    knowExtensions.map(ext => {
+        if (found) return;
+
+        let indexOf = fileName.lastIndexOf('.' + ext);
+
+        if (indexOf > 0) {
+            reg[0] = fileName.substring(0, indexOf);
+            reg[1] = fileName.substring(indexOf + 1);
+            found = true;
+        }
+    });
+
+    if (found) return reg;
+
+    let indexOf = fileName.lastIndexOf('.');
+
+    if (indexOf > 0) {
+        reg[0] = fileName.substring(0, indexOf);
+        reg[1] = fileName.substring(indexOf + 1);
+    } else {
+        reg[0] = fileName;
+    }
+
+    return reg;
+}
+
+/**
+ * Install a new version of environment
+ * 
+ * @param {object} engine - The environment engine
+ * @param {string} version - Version number
+ * @param {string} installPath - Path of installation
+ */
+function installNewVersion(engine, version, installPath) {
+    let tempDir = _dev.generateTempDir(),
+        extractedDir = _path.join(tempDir, 'extracted');
+
+    try {
+        let getDownloadFileListFn = engine['getDownloadFileList'];
+
+        if (typeof (getDownloadFileListFn) != 'function') {
+            throw _dev.createError('Environment '
+                + engine.name.toUpperCase() + ' does not implements getDownloadFileList() method.');
+        }
+
+        let downloadFileList = getDownloadFileListFn.bind(engine)(version);
+
+        if (!Array.isArray(downloadFileList) || downloadFileList.length < 1) {
+            throw _dev.createError('Environment '
+                + engine.name.toUpperCase()
+                + '.getDownloadFileList() returned an invalid download file list.');
+        }
+
+        // Creating temporary directory
+        _dev.mkdir(tempDir);
+        _dev.mkdir(extractedDir);
+
+        // Download files to temporary directory
+        _dev.printf('Downloading files...');
+
+        let downloadedFiles = [];
+
+        for (let fileIdx = 0; fileIdx < downloadFileList.length; fileIdx++) {
+            let fileUrl = downloadFileList[fileIdx],
+                fileName = fileUrl.substring(fileUrl.lastIndexOf('/') + 1),
+                filePath = _path.join(tempDir, fileName);
+
+            _dev.printf('  ->', fileName);
+            _dev.downloadSync(fileUrl, filePath);
+            downloadedFiles.push(fileName);
+        }
+
+        // Extract files
+        _dev.printf('Extracting files...');
+
+        for (let fileIdx = 0; fileIdx < downloadedFiles.length; fileIdx++) {
+            let fileName = downloadedFiles[fileIdx],
+                filePath = _path.join(tempDir, fileName),
+                fileNameInfo = splitFileNameExtension(fileName),
+                fileExtractDir = _path.join(extractedDir, fileNameInfo[0]);
+
+            if (fileNameInfo[1] && ['zip', 'tar.gz'].indexOf(fileNameInfo[1])) {
+                _dev.printf('  ->', fileName);
+                _dev.extractFile(filePath, fileExtractDir);
+            }
+        }
+
+        let installFilesFn = engine['installFiles'];
+
+        if (typeof (installFilesFn) != 'function') {
+            throw _dev.createError('Environment '
+                + engine.name.toUpperCase() + ' does not implements installFiles() method.');
+        }
+
+        _dev.printf('Installing files...');
+
+        // Creating installation directory
+        _dev.mkdir(installPath);
+
+        installFilesFn.bind(engine)(tempDir, extractedDir, installPath, version);
+
+        _dev.printf('Cleaning temporary files...');
+
+        // Cleaning temporary directory
+        if (_dev.directoryExists(tempDir)) {
+            _dev.rmdir(tempDir);
+        }
+    } catch (error) {
+        // Cleaning directories
+
+        if (_dev.directoryExists(installPath)) {
+            _dev.rmdir(installPath);
+        }
+
+        if (_dev.directoryExists(tempDir)) {
+            _dev.rmdir(tempDir);
+        }
+
+        throw error;
+    }
 }
 
 /**
@@ -153,26 +562,35 @@ class Environment extends _dev.DevCom {
             return;
         }
 
-        let envLib;
+        let envEngine;
 
         if (options.devmode && (process.env['DEVCOM_MODE'] || '').toUpperCase() === 'DEVELOPMENT') {
             let devModulePath = _path.join(process.cwd(), './src/lib/env/' + env);
-            envLib = require(devModulePath);
+            envEngine = require(devModulePath);
         } else {
-            envLib = _dev.require('lib://env/' + env);
+            envEngine = _dev.require('lib://env/' + env);
         }
 
-        if (envLib.name !== env) {
+        if (envEngine.name !== env) {
             throw _dev.createError(env.toUpperCase()
                 + ' environment with invalid name.');
         }
 
+        let initFn = envEngine['init'];
+
+        if (typeof (initFn) != 'function') {
+            throw _dev.createError('Environment '
+                + env.toUpperCase() + ' does not implements init() method.');
+        }
+
+        initFn.bind(envEngine)(devTool, options);
+
         if (ALIAS[action]) {
-            this.runEnvCommonEngine(envLib, action, devTool, options);
+            this.runEnvCommonEngine(envEngine, action, devTool, options);
             return;
         }
 
-        this.runEnvEngine(envLib, action, devTool, options);
+        this.runEnvEngine(envEngine, action, devTool, options);
     }
 
     /**
@@ -212,14 +630,6 @@ class Environment extends _dev.DevCom {
                 + actionName.toUpperCase() + ' action.');
         }
 
-        let initFn = engine['init'];
-
-        if (typeof (initFn) != 'function') {
-            throw _dev.createError('Environment '
-                + engine.name.toUpperCase() + ' does not implements init() method.');
-        }
-
-        initFn.bind(engine)(devTool, options);
         actionFn.bind(engine)();
     }
 
@@ -242,6 +652,7 @@ class Environment extends _dev.DevCom {
      * @param {object} options - Options for arguments of command
      */
     installCommonAction(engine, devTool, options) {
+        // Load version info from cache
         let versionCacheInfo = loadVersionCacheInfo(engine.name);
 
         if (!versionCacheInfo) {
@@ -249,14 +660,61 @@ class Environment extends _dev.DevCom {
 
             if (typeof (getVersionsFn) != 'function') {
                 throw _dev.createError('Environment '
-                    + engine.name.toUpperCase() + ' does not implements getVersions() method.');
+                    + engine.name.toUpperCase()
+                    + ' does not implements getVersions() method.');
             }
 
             versionCacheInfo = saveVersionCacheInfo(engine.name, getVersionsFn.bind(engine)());
         }
 
-        throw 'Not implemented [installCommonAction]';
+        if (!versionCacheInfo) {
+            throw _dev.createError('Error on retrieve '
+                + engine.name.toUpperCase() + ' versions information.');
+        }
+
+        // Find version info
+        let version = options.args[0] || 'latest',
+            fullVersion = getFullVersionNumber(version, versionCacheInfo, options, engine);
+
+        if (!fullVersion) {
+            throw _dev.createError(engine.name.toUpperCase() + ' version "'
+                + version + '" not found.');
+        }
+
+        let installDirectoryPath = makeInstallDirectoryPath(fullVersion, engine.name);
+
+        // Clearing an incomplete installation directory if necessary
+        if (!_dev.directoryIsEmpty(installDirectoryPath)) {
+            let successfullyInstalledFn = engine['successfullyInstalled'];
+
+            if (typeof (successfullyInstalledFn) != 'function') {
+                throw _dev.createError('Environment '
+                    + engine.name.toUpperCase()
+                    + ' does not implements successfullyInstalled() method.');
+            }
+
+            if (!successfullyInstalledFn.bind(engine)(fullVersion, installDirectoryPath)) {
+                _dev.rmdir(installDirectoryPath);
+            }
+        }
+
+        if (_dev.directoryExists(installDirectoryPath)) {
+            if (!_dev.directoryIsEmpty(installDirectoryPath)) {
+                _dev.printf(engine.name.toUpperCase(),
+                    'version "' + fullVersion + '" already installed.');
+                return;
+            }
+
+            _dev.rmdir(installDirectoryPath);
+        }
+
+        installNewVersion(engine, fullVersion, installDirectoryPath);
+
+        _dev.printf(engine.name.toUpperCase(),
+            'version "' + fullVersion + '" installed successfully!');
     }
+
+
 
     /**
      * Uninstall `env uninstall ...` common action
@@ -266,7 +724,7 @@ class Environment extends _dev.DevCom {
      * @param {object} options - Options for arguments of command
      */
     uninstallCommonAction(engine, devTool, options) {
-        throw 'Not implemented [uninstallCommonAction]';
+        throw _dev.createError('Not implemented [uninstallCommonAction]');
     }
 
     /**
@@ -277,7 +735,7 @@ class Environment extends _dev.DevCom {
      * @param {object} options - Options for arguments of command
      */
     listCommonAction(engine, devTool, options) {
-        throw 'Not implemented [listCommonAction]';
+        throw _dev.createError('Not implemented [listCommonAction]');
     }
 
     /**
@@ -288,7 +746,7 @@ class Environment extends _dev.DevCom {
      * @param {object} options - Options for arguments of command
      */
     selectCommonAction(engine, devTool, options) {
-        throw 'Not implemented [selectCommonAction]';
+        throw _dev.createError('Not implemented [selectCommonAction]');
     }
 
     /**
@@ -299,7 +757,7 @@ class Environment extends _dev.DevCom {
      * @param {object} options - Options for arguments of command
      */
     testCommonAction(engine, devTool, options) {
-        throw 'Not implemented [testCommonAction]';
+        throw _dev.createError('Not implemented [testCommonAction]');
     }
 
     /**
--- src/lib/env/node.js
+++ src/lib/env/node.js
@@ -6,6 +6,7 @@
 
 let _dev = require('e5r-dev'),
     _path = require('path'),
+    _fs = require('fs'),
     _os = require('os');
 
 /**
@@ -20,7 +21,36 @@ const SUPPORTED_PLATFORMS = [
 ];
 
 /** @constant {string} */
+const CHMOD_EXECUTABLE = '100750';
+
+/** @constant {string} */
 const NODE_VERSION_INDEX_URL = "https://nodejs.org/dist/index.json";
+
+/**
+ * Get a current architecture
+ */
+function getArchitecture() {
+    let arch = _os.arch();
+
+    if (arch === 'ia32') {
+        return 'x86';
+    }
+
+    return arch;
+}
+
+/**
+ * Get a current platform
+ */
+function getPlatformPrefix() {
+    let platform = _os.platform();
+
+    if (platform === 'win32') {
+        return 'win';
+    }
+
+    return platform;
+}
 
 /**
  * Management NODE Environment
@@ -35,11 +65,6 @@ class NodeEnvironment {
         }
     }
 
-    /** @required */
-    get name() {
-        return 'node';
-    }
-
     get devTool() {
         return this._devTool;
     }
@@ -49,9 +74,124 @@ class NodeEnvironment {
     }
 
     /** @required */
+    get name() {
+        return 'node';
+    }
+
+    /** @required */
     init(devTool, options) {
         this._devTool = devTool;
         this._options = options;
+    }
+
+    /** @required */
+    getFullVersionNumber(version) {
+        return version;
+
+        /** @todo: Implements check options */
+    }
+
+    /** @required */
+    versionIsValid(version) {
+        return true;
+
+        /** @todo: Implements check options */
+    }
+
+    /**
+     * Make a download file list for a new installation
+     * 
+     * @required
+     * 
+     * @param {string} version - Version number
+     *
+     * @return {string[]} Array of URL to download
+     * */
+    getDownloadFileList(version) {
+        let filesToDownload = [],
+            arch = this.options.arch || getArchitecture();
+
+        // Darwin, Linux, Sunos, AIX, and Windows (only node 6.2.1 or higher)
+        let platform = getPlatformPrefix(),
+            pkgSufix = platform === 'win' ? 'zip' : 'tar.gz',
+            fileName = 'node-v{version}-{platform}-{arch}.{pkg}'
+                .replace('{version}', version)
+                .replace('{platform}', platform)
+                .replace('{arch}', arch)
+                .replace('{pkg}', pkgSufix),
+            url = 'https://nodejs.org/dist/v{version}/{file}'
+                .replace('{version}', version)
+                .replace('{file}', fileName);
+
+        /** @todo: Implements windows filename and npm url if required */
+
+        filesToDownload.push(url);
+
+        return filesToDownload;
+    }
+
+    /**
+     * Install files to installation folder
+     * 
+     * @param {string} downloaddedPath - Downloadded directory path
+     * @param {string} extractedPath - Extracted files directory path
+     * @param {string} installPath - Installation directory path
+     * @param {string} version - Version number
+     */
+    installFiles(downloaddedPath, extractedPath, installPath, version) {
+        /** @todo: Check all versions format files and directories */
+
+        let arch = this.options.arch || getArchitecture(),
+            platform = getPlatformPrefix(),
+            folderName = 'node-v{version}-{platform}-{arch}'
+                .replace('{version}', version)
+                .replace('{platform}', platform)
+                .replace('{arch}', arch);
+
+        _dev.copyDirectory(_path.join(extractedPath, folderName), installPath);
+
+        // Set executable flag for *nix
+        if (platform !== 'win') {
+            let nodeExePath = _path.join(installPath, 'bin', 'node'),
+                npmExePath = _path.join(installPath, 'bin', 'npm');
+
+            if (_dev.fileExists(nodeExePath)) {
+                _fs.chmodSync(nodeExePath, CHMOD_EXECUTABLE);
+            }
+
+            if (_dev.fileExists(npmExePath)) {
+                _fs.chmodSync(npmExePath, CHMOD_EXECUTABLE);
+            }
+        }
+    }
+
+    /**
+     * Verify installation files
+     * 
+     * @param {string} version - Version number
+     * @param {string} installPath - Installation path
+     * 
+     * @return {boolean} True if OK, or False.
+     */
+    successfullyInstalled(version, installPath) {
+        let platform = getPlatformPrefix(),
+            nodeExe = platform === 'win' ? 'node.exe' : 'node',
+            npmExe = platform === 'win' ? 'npm.cmd' : 'npm',
+            nodePath = _path.join(installPath, 'bin', nodeExe),
+            npmPath = _path.join(installPath, 'bin', npmExe),
+            modulesPath = _path.join(installPath, 'lib', 'node_modules');
+
+        // files and directories
+        if (!_dev.fileExists(nodePath)) return false;
+        if (!_dev.fileExists(npmPath)) return false;
+        if (!_dev.directoryExists(modulesPath)) return false;
+
+        // chmod
+        if (_fs.statSync(nodePath).mode !== parseInt(CHMOD_EXECUTABLE, 8)) return false;
+        if (_fs.statSync(npmPath).mode !== parseInt(CHMOD_EXECUTABLE, 8)) return false;
+
+        // Successfully
+        return true;
     }
 
     /** @required */
